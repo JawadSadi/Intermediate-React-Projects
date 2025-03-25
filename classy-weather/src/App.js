05a106091a0d5dc8851a9547bd11e4c513a278d2
Jawad <Jawadsadiq7373@gmail.com>
2025-03-25 00:29:44 -0700

    fetching weather data from API

--- classy-weather/src/App.js
+++ classy-weather/src/App.js
@@ -1,14 +1,57 @@
+//1
+
 import React from "react";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { location: "lisbon", isLoading: false };
+    this.fetchWeather = this.fetchWeather.bind(this);
+  }
+
+  async fetchWeather() {
+    this.setState({ isLoading: true });
+    try {
+      // 1) Getting location (geocoding)
+      const geoRes = await fetch(
+        `https://geocoding-api.open-meteo.com/v1/search?name=${this.state.location}`
+      );
+      const geoData = await geoRes.json();
+      console.log(geoData);
+
+      if (!geoData.results) throw new Error("Location not found");
+
+      const { latitude, longitude, timezone } = geoData.results.at(0);
+
+      // 2) Getting actual weather
+      const weatherRes = await fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
+      );
+      const weatherData = await weatherRes.json();
+      console.log(weatherData.daily);
+    } catch (err) {
+      console.err(err);
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  }
+
   render() {
     return (
       <div className="app">
         <h1>Classy Weather</h1>
         <div>
-          <input type="text" placeholder="Search For Location..." />
+          <input
+            type="text"
+            placeholder="Search For Location..."
+            value={this.state.location}
+            onChange={(e) => this.setState({ location: e.target.value })}
+          />
         </div>
-        <button>Get weather</button>
+        <button onClick={this.fetchWeather}>Get weather</button>
+
+        {this.state.isLoading && <p className="loader">Loadin...</p>}
       </div>
     );
   }
