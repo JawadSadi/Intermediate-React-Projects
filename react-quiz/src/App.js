4b9d3f699b96265acb08648cfd287aa4e9610a81
Jawad <Jawadsadiq7373@gmail.com>
2025-03-27 12:13:20 -0700

    Made my own data and fetch by a fake API

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,7 +1,14 @@
 import Header from "./Header";
 import Main from "./Main";
+import { useEffect } from "react";
 
 export default function App() {
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error(err));
+  }, []);
   return (
     <div className="app">
       <Header />
