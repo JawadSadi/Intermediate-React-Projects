d023b4fbbc01fcf0fda8b0a2eabe6a381db8642a
Jawad <Jawadsadiq7373@gmail.com>
2025-03-27 21:38:12 -0700

    show the questions in diffrent status using useReducer

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,20 +1,46 @@
 import Header from "./Header";
 import Main from "./Main";
-import { useEffect } from "react";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import { useEffect, useReducer } from "react";
+
+const initialState = {
+  questions: [],
+
+  //loading , error , ready , active , finshed
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("unknown action");
+  }
+}
 
 export default function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
+
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.error(err));
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
   }, []);
   return (
     <div className="app">
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Questions?</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
       </Main>
     </div>
   );
